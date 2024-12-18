6baf48d3540a33d2103f21e749b4bc25fcb43e56
Roman505050 <myhun59@gmail.com>
2024-12-19 01:25:22 +0200

    Add GitHub Actions workflows for Docusaurus deployment and testing

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -19,6 +19,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'nesharp', // Usually your GitHub org/user name.
   projectName: 'weiqi', // Usually your repo name.
+  deploymentBranch: 'master',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
